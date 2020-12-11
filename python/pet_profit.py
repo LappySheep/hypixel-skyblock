80287c851fb750848fb8ee5acc844c7b8c2c37fc
LappySheep <lappysheep@gmail.com>
2020-12-11 16:59:03 +0000

    Update pet_profit.py

--- python/pet_profit.py
+++ python/pet_profit.py
@@ -15,12 +15,46 @@ def main():
     e = input("Enter epic pet sell price (k, m accepted): ")
     l = input("Enter leg pet sell price (k, m accepted): ")
     c,e,l = [km(x)for x in [c,e,l]]
-    pl = int(((400 / (l/e - 1)) - 100) // 1)
+    try:
+        pl = -((100*(l + 4*e - 5*c))/(l-c))
+    except ZeroDivisionError:
+        print("The cost of crafting a pet has to be lower than the price of a legendary variant.")
+        return
 
-    print("You would need {} Pet Luck to break even, or {} Pet Luck to make profit.".format(pl, pl+1))if pl>0else print("You break even no matter what.")
+    if pl <= 0:
+        print("Guaranteed profit no matter what.")
+    elif pl > 0:
+        print(f"You would need more than {int(pl)} Pet Luck to make profit on average.")
 
 
 
 while __name__ == "__main__":
     print("\n\n")
     main()
+
+
+"""
+(notes)
+base chance
+20 : 80
+
+E/L = amount the Epic/Leg pets sell for
+C = craft cost
+
+lw = 20 * (1+(P/100))
+lc = lw / (80+lw)
+    = (20 * (1+(P/100))) / (80+(20 * (1+(P/100))))
+
+X = lc*L + (1-lc)*E
+
+to break even
+    let n = lc
+    nL + (1-n)E = C
+    n = (C-E)/(L-E)
+    (20 * (1+(P/100))) / (80+(20 * (1+(P/100)))) = (C-E)/(L-E)
+
+    p = -((100*(L + 4*E - 5*C))/(L-C))
+
+
+craft cost has to be lower than leg price
+"""
