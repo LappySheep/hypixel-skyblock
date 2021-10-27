73c806a1c7a3d6758f162977ad0b96483106fa1b
Lily <lappysheep@gmail.com>
2021-10-27 19:23:05 +0100

    updated to actually mathematically be correct
    note that this still uses the same ideology - where pet luck multiplies the weight, not the chance
    
    this is not a wiki leak, this is a revamp of an old formula which didn't work

--- python/pet_profit.py
+++ python/pet_profit.py
@@ -1,60 +1,54 @@
 def km(s):
-    if s[-1] == "k":
-        s = int(s[:-1]) * 1000
-    elif s[-1] == "m":
-        s = int(s[:-1]) * 1000000
-    try:
-        return int(s)
-    except:
-        print("Invalid value.")
-        return
+  if s[-1] == "k":
+    s = int(s[:-1]) * 1000
+  elif s[-1] == "m":
+    s = int(s[:-1]) * 1000000
+  try:
+    return int(s)
+  except:
+    return False
+
+
+def pet_profit(crafting_price, epic_sell, leg_sell):
+  if leg_sell < crafting_price and epic_sell < crafting_price:
+    return "ilp"
+  loop, iter = True, 0
+  while loop:
+    pet_luck = iter
+    lc = (1 + (pet_luck / 100)) * 20
+    leg_pet = lc / (80 + lc)
+    epic_pet = 1 - leg_pet
+    gain = leg_pet * leg_sell + epic_pet * epic_sell
+    profit = gain - crafting_price
+    if profit < 0:
+      iter += 1
+      #print(f"DEBUG: Pet Luck: {pet_luck} ||| Profit: {profit}")
+    else:
+      loop = False
+      return pet_luck
 
 
 def main():
-    c = input("Enter cost of crafting 1 pet (k, m accepted): ")
-    e = input("Enter epic pet sell price (k, m accepted): ")
-    l = input("Enter leg pet sell price (k, m accepted): ")
-    c,e,l = [km(x)for x in [c,e,l]]
-    try:
-        pl = -((100*(l + 4*e - 5*c))/(l-c))
-    except ZeroDivisionError:
-        print("The cost of crafting a pet has to be lower than the price of a legendary variant.")
-        return
+  print("(k/m suffixes allowed)")
+  c = input("Enter cost of crafting 1 pet: ")
+  e = input("Enter epic pet sell price: ")
+  l = input("Enter leg pet sell price: ")
+  c,e,l = [km(x)for x in [c,e,l]]
+  if not all([c,e,l]):
+    print("Invalid arguments.")
+    return
 
-    if pl <= 0:
-        print("Guaranteed profit no matter what (on average).")
-    elif pl > 0:
-        print(f"You would need more than {int(pl)} Pet Luck to make profit on average.")
+  pl = pet_profit(c, e, l)
+  if pl == "ilp":
+    print("Crafting price must be lower than the sale price either the legendary or epic variant.")
+    return
+  if pl == 0:
+    print("Guaranteed profit no matter what (on average).")
+  elif pl > 0:
+    print(f"You would need more than {pl} Pet Luck to make profit on average.")
 
 
 
 while __name__ == "__main__":
-    print("\n\n")
-    main()
-
-
-"""
-(notes)
-base chance
-20 : 80
-
-E/L = amount the Epic/Leg pets sell for
-C = craft cost
-
-lw = 20 * (1+(P/100))
-lc = lw / (80+lw)
-    = (20 * (1+(P/100))) / (80+(20 * (1+(P/100))))
-
-X = lc*L + (1-lc)*E
-
-to break even
-    let n = lc
-    nL + (1-n)E = C
-    n = (C-E)/(L-E)
-    (20 * (1+(P/100))) / (80+(20 * (1+(P/100)))) = (C-E)/(L-E)
-
-    p = -((100*(L + 4*E - 5*C))/(L-C))
-
-
-craft cost has to be lower than leg price
-"""
+  print("\n\n")
+  main()
