a2f514e3cd1cfa9da1ff499b46e56df8fc8124bd
Lily <lappysheep@gmail.com>
2022-04-03 09:02:04 +0100

    Update midas.py

--- python/midas.py
+++ python/midas.py
@@ -1,3 +1,3 @@
 import random as z
 def midas():
- if z.choices([1,0],weights=[1,99]):r,l=10**4,5000;A=z.randint(0,9999);return z.randint(*[(100*r,l*r),(25*r,100*r),(5*r,25*r),(l,5*r),(1000,l),(100,1000)][min([i if(A<b)else 9for i,b in enumerate([1,9,90,400,2500,r])])])
+ if z.choices([1,0],weights=[1,99])[0]:r,l=10**4,5000;A=z.randint(0,9999);return z.randint(*[(100*r,l*r),(25*r,100*r),(5*r,25*r),(l,5*r),(1000,l),(100,1000)][min([i if(A<b)else 9for i,b in enumerate([1,9,90,400,2500,r])])])
